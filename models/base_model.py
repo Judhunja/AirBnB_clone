4f90141fa84dee58704f7ab31ab291d5c53f5898
Judhunja <judxhunja@gmail.com>
2023-08-07 21:47:57 +0300

    recreating an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,23 @@ from datetime import datetime
 class BaseModel:
     """Initializes a class BaseModel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Init method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = None
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    if value is not None:
+                        format = "%Y-%m-%dT%H:%M:%S.%f"
+                        setattr(self, key, datetime.strptime(value, format))
+                    else:
+                        setattr(self, key, None)
+                else:
+                    if key != "__class__":
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints string representation of the class"""
@@ -19,13 +31,15 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with the current datetime"""
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dict containing all key/value pairs of the dict
         representation of the instance, including the class name of the object
+        created_at and updated_at are returned in isoformat(readable format)
         """
         dict_obj = self.__dict__
+        dict_obj["created_at"] = dict_obj["created_at"].isoformat()
+        dict_obj["updated_at"] = dict_obj["updated_at"].isoformat()
         dict_obj["__class__"] = __class__.__name__
         return dict_obj
-
